1acaa0d2f68f17100018989fa5bb041c7ba0de0c
mohanbalaji <mohanbalajim29@gmail.com>
2018-06-25 01:03:20 +0530

    product

---
+++ models/product.js
@@ -0,0 +1,15 @@
+const knex = require('../libs/db.js');
+
+function getProduct(){
+  
+    
+    
+    
+    return knex.select()
+        .from('products')
+        .limit(20)
+}
+
+module.exports = {
+    getProduct:getProduct
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+const cm = require('csv-mysql');
+const express = require('express');
+
+const product = require('./models/product');
+
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+}) 
+
+
+app.post('/list', function (req, res) {
+  product.getProduct()
+        .then(function (rows){
+          res.json(rows)
+        });
+})
+
+app.listen(3000)
+
+
